22cb225c88ebe2aeb4dea345250a5e5078fbed49
israel-lissak <islissak@gmail.com>
2023-08-16 18:45:48 +0300

    fourth commit

--- app.js
+++ app.js
@@ -1,75 +1,130 @@
-const { json } = require('body-parser');
-const express = require('express');
+const { json } = require("body-parser");
+const express = require("express");
 const app = express();
 const port = 3000;
-const { v4: uuidv4 } = require('uuid');
-const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jsonfile = require("jsonfile");
+const axios = require("axios");
 
-app.use(express.json())
+app.use(express.json());
 
 // get all users
-app.get('/', (req, res) => {
-    res.send(users);
+app.get("/", (req, res) => {
+    res.send(jsonfile.readFileSync("usersDB.json"));
 });
 
 // get user by id
-app.get('/:id', (req, res) => {
-    let user = users.filter(e => e.id == req.params.id);
+app.get("/:id", (req, res) => {
+    const allUsers = jsonfile.readFileSync("usersDB.json");
+    let user = allUsers.filter((e) => e.id == req.params.id);
     console.log(req.params.id);
     console.log(user);
     res.send(user);
 });
 
-
 // add new user
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
     const rndomId = uuidv4();
-    let user = users.filter(e => e.id == rndomId);
+    const allUsers = jsonfile.readFileSync("usersDB.json");
+    let user = allUsers.filter((e) => e.id == rndomId);
     console.log(user, user.length);
     if (user.length > 0) {
-        res.send('id exist')
+        res.send("id exist");
+    } else if (!validator.isEmail(req.body.email)) {
+        res.send("email is not valid");
+    } else if (!validator.isStrongPassword(req.body.password)) {
+        res.send("password is not stront enough");
     } else {
-        const obj = {id: rndomId , email: req.body.email, password: bcrypt.hashSync(req.body.password, 10)};
-        users.push(obj);
-        res.send('user added succesfuly');
+        const obj = {
+            id: rndomId,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+        };
+        allUsers.push(obj);
+        jsonfile.writeFileSync("usersDB.json", allUsers);
+        res.send("user added succesfuly");
     }
-})
+});
 
 // log in Validation
-app.post('/:email/:password', (req, res) => {
+app.post("/:email/:password", (req, res) => {
     let userFound = false;
-    users.forEach(element => {
-        if (req.params.email === element.email & bcrypt.compareSync(req.params.password, element.password)) {
-            res.send('User is connected');
+    const allUsers = jsonfile.readFileSync("usersDB.json");
+    allUsers.forEach((element) => {
+        if (
+            (req.params.email === element.email) &
+            bcrypt.compareSync(req.params.password, element.password)
+        ) {
+            res.send("User is connected");
             userFound = true;
-        } 
+        }
     });
     if (!userFound) {
-        res.send('wrong credentials')
+        res.send("wrong credentials");
     }
-})
+});
 
 // update user (by id)
-app.put('/:id', (req, res) => {
-    let user = users.filter(e => e.id == req.params.id);
-    const obj = {id: req.body.id, email: req.body.email, password: req.body.password};
-    users[user] = obj;
-    res.send(users)
+app.put("/:id", (req, res) => {
+    const allUsers = jsonfile.readFileSync("usersDB.json");
+    let user = allUsers.filter((e) => e.id == req.params.id);
+    const obj = {
+        id: req.body.id,
+        email: req.body.email,
+        password: req.body.password,
+    };
+    allUsers[user] = obj;
+    jsonfile.writeFileSync("usersDB.json", allUsers);
+    res.send(allUsers);
 });
 
 // delete user (by id)
-app.delete('/:id', (req, res) => {
-    let user = users.filter(e => e.id == req.params.id);
+app.delete("/:id", (req, res) => {
+    const allUsers = jsonfile.readFileSync("usersDB.json");
+    let user = allUsers.filter((e) => e.id == req.params.id);
     console.log(user, user.length);
     if (user.length === 0) {
-        res.send('id not exist')
+        res.send("id not exist");
     } else {
-       users.splice(users.filter(e => e.id == req.params.id), 1);
-        console.log(users);
-        res.send('user delete succesfuly');
+        allUsers.splice(
+            allUsers.filter((e) => e.id == req.params.id),
+            1
+        );
+        console.log(allUsers);
+        jsonfile.writeFileSync("usersDB.json", allUsers);
+        res.send("user delete succesfuly");
     }
-})
+});
 
+axios.get("https://dummyjson.com/products/1")
+    .then((response) => {
+        const data = response.data;
+        console.log(data);
+        const allUsers = jsonfile.readFileSync("usersDB.json");
+        const user = allUsers[0];
+        console.log(user);
+        user.prodact = data;
+        console.log(user);
+        return axios.post("https://jsonplaceholder.typicode.com/users", user);
+    })
+    .then((response) => {
+        const responseData = response.data;
+        console.log(responseData);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+    
+axios.get("https://jsonplaceholder.typicode.com/users/11")
+    .then((response) => {
+        const data = response.data;
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
 
 // 'server is running' messege
 app.listen(port, () => {
@@ -78,12 +133,19 @@ app.listen(port, () => {
 
 // demo array
 
-const hashedPassword1 = bcrypt.hashSync('abc111:)', 10);
-const hashedPassword2 = bcrypt.hashSync('abc222:)', 10);
-const hashedPassword3 = bcrypt.hashSync('abc333:)', 10);
+const hashedPassword1 = bcrypt.hashSync("Abc111:)", 10);
+const hashedPassword2 = bcrypt.hashSync("Abc222:)", 10);
+const hashedPassword3 = bcrypt.hashSync("Abc333:)", 10);
 const users = [
-    {id: uuidv4(), email: 'abc1@sample.com', password: hashedPassword1},
-    {id: uuidv4(), email: 'abc2@sample.com', password: hashedPassword2},
-    {id: uuidv4(), email: 'abc3@sample.com', password: hashedPassword3},
-]
+    { id: uuidv4(), email: "abc1@sample.com", password: hashedPassword1 },
+    { id: uuidv4(), email: "abc2@sample.com", password: hashedPassword2 },
+    { id: uuidv4(), email: "abc3@sample.com", password: hashedPassword3 },
+];
+
+jsonfile.writeFileSync("usersDB.json", users);
+
+const usresArr = jsonfile.readFileSync("usersDB.json");
+console.log(usresArr);
+
+
 
